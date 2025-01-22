b814b5cf2b3b3b812b3e80c540de558a9f60ab1b
Edward Deleu <edeleu@princeton.edu>
2025-01-22 15:04:04 -0500

    Add files via upload
    add working, semi-optimized code

---
+++ precompute.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+PI = np.pi
+IMAG = 1j
+
+# A helper function to precompute valid n-values to iterate through when constructing the Hamiltonian
+# in accordance with the periodic boundary conditions
+def precompute_n_PBC(N, size):
+    lookup_table = {}
+
+    for j in range(N):  # Loop through all values of j
+        for k in range(N):  # Loop through all values of k
+            valid_n = []  # List to store valid n for the current (j, k)
+
+            for n in range(-size, size + 1):  # Loop through the range of n
+                # Compute modulus and check if it equals 0... Delta function!
+                modulus = (j - (k - n)) % N
+                if modulus == 0:
+                    valid_n.append(n)
+
+            lookup_table[(j, k)] = valid_n  # Store the valid n values for (j, k)
+
+    return lookup_table
+
+# A helper function to precompute each 2-variable section of the Hamiltonian exponential
+# ie, for mn, mj, mthetaY, nThetaX, and mj.
+def precompute_exponentials(N, Vsize, numTheta):
+    thetas = np.linspace(0, 2*PI, num=numTheta, endpoint=True)
+
+    mn_LUT = {}
+    for n in range(-Vsize, Vsize+1):
+        for m in range(-Vsize, Vsize+1):
+            mn_LUT[(m,n)] = np.exp(-PI*(m**2+n**2)/(2*N)-IMAG*PI*m*n/N)
+
+    mj_LUT = {}
+    for indexJ in range(N):
+        for m in range(-Vsize, Vsize+1):
+            mj_LUT[(m,indexJ)] = np.exp(-IMAG*2*PI*m*indexJ/N)
+
+    mThetaY_LUT = {}
+    for m in range(-Vsize, Vsize+1):
+        for thetaIndex in range(numTheta):
+            thetaY = thetas[thetaIndex]
+            mThetaY_LUT[(m,thetaIndex)] = np.exp(IMAG*(m*thetaY)/N)
+
+    nThetaX_LUT = {}
+    for n in range(-Vsize, Vsize+1):
+        for thetaIndex in range(numTheta):
+            thetaX = thetas[thetaIndex]
+            nThetaX_LUT[(n,thetaIndex)] = np.exp(-IMAG*(n*thetaX)/N)
+    
+    return mn_LUT, mThetaY_LUT, nThetaX_LUT, mj_LUT
+
+# An updated helper function that computes 2 look-up tables for the Hamiltonian exponential
+# for small systems, it's quicker to use mnj look-up table instead of two
+# Furthermore, mTheta can be used for nTheta so both are not computed.
+def precompute_exponentialsV2(N, Vsize, numTheta):
+    thetas = np.linspace(0, 2*PI, num=numTheta, endpoint=True)
+
+    mnj_LUT = {}
+    for indexJ in range(N):
+        for n in range(-Vsize, Vsize+1):
+            for m in range(-Vsize, Vsize+1):
+                mnj_LUT[(m,n,indexJ)] = np.exp(-PI*(m**2+n**2)/(2*N)-IMAG*PI*m*n/N-IMAG*2*PI*m*indexJ/N)
+
+    mTheta_LUT = {}
+    for m in range(-Vsize, Vsize+1):
+        for thetaIndex in range(numTheta):
+            theta = thetas[thetaIndex]
+            mTheta_LUT[(m,thetaIndex)] = np.exp(IMAG*(m*theta)/N)
+    
+    return mnj_LUT, mTheta_LUT
+
+# A different implementation to precompute the exponentials for the Hamiltonian, safe with Numba
+# instead of using dictionary, use 3 NumPy matrices for lookup.
+def precompute_exponentialsNumPy(N, Vsize, numTheta):
+    thetas = np.linspace(0, 2*PI, num=numTheta, endpoint=True)
+    
+    mn_LUT =  np.zeros((2*Vsize+1,2*Vsize+1),dtype=np.complex128)
+    for n in range(-Vsize, Vsize+1):
+        for m in range(-Vsize, Vsize+1):
+            mn_LUT[Vsize+m,Vsize+n] = np.exp(-PI*(m**2+n**2)/(2*N)-IMAG*PI*m*n/N)
+
+    mj_LUT =  np.zeros((2*Vsize+1,N),dtype=np.complex128)
+    for indexJ in range(N):
+        for m in range(-Vsize, Vsize+1):
+            mj_LUT[Vsize+m,indexJ] = np.exp(-IMAG*2*PI*m*indexJ/N)
+
+    # MNJ is slower for >256 systems, don't use this
+    # mnj_LUT = np.zeros((2*Vsize+1,2*Vsize+1,N),dtype=np.complex128)
+    # for indexJ in range(N):
+    #     for n in range(-Vsize, Vsize+1):
+    #         for m in range(-Vsize, Vsize+1):
+    #             mnj_LUT[m+Vsize,n+Vsize,indexJ] = np.exp(-PI*(m**2+n**2)/(2*N)-IMAG*PI*m*n/N-IMAG*2*PI*m*indexJ/N)
+    #             # mnj_LUT[(m,n,indexJ)] = np.exp(-PI*(m**2+n**2)/(2*N)-IMAG*PI*m*n/N-IMAG*2*PI*m*indexJ/N)
+    
+    mTheta_LUT = np.zeros((2*Vsize+1,numTheta),dtype=np.complex128)
+    for m in range(-Vsize, Vsize+1):
+        for thetaIndex in range(numTheta):
+            theta = thetas[thetaIndex]
+            mTheta_LUT[m+Vsize,thetaIndex] = np.exp(IMAG*(m*theta)/N)
+    return mn_LUT, mj_LUT, mTheta_LUT
+
+# if __name__ == "__main__":
+#     # Perform any testing here...
---
+++ simulation.py
@@ -0,0 +1,319 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import precompute
+import timeit
+from numba import njit, prange, jit
+import scipy
+
+# Define system-wide parameters...
+# num-states is defined according to the ratio  
+# N= B*L^2 / phi_o for phi_o=hc/e OR
+# N = A / (2pi ell^2)=> so L^2 = 2pi*ell^2*N
+# for ell^2 = hbar c / (e*B)
+
+PI = np.pi
+NUM_STATES = 256
+NUM_THETA=26
+IMAG = 1j
+POTENTIAL_MATRIX_SIZE = int(4*np.sqrt(NUM_STATES))
+# basically experimental parameter, complete more testing with this size... (keep L/M small)
+
+# Precompute look-up tables (matrices) for Hamiltonian exponentials
+mn_LUT, mj_LUT, mTheta_LUT = precompute.precompute_exponentialsNumPy(NUM_STATES, POTENTIAL_MATRIX_SIZE, NUM_THETA)
+
+
+# Construct the Hamiltonian Matrix for a given Theta, Num_States, and random-potential.
+# note: "theta" is actually an index here for efficiency
+@njit(parallel=True,fastmath=True)
+def constructHamiltonian(matrix_size, theta_x, theta_y, matrixV):
+    # define values as complex128, ie, double precision for real and imaginary parts
+    H = np.zeros((matrix_size,matrix_size),dtype=np.complex128)
+
+    # actually, we only require the lower-triangular portion of the matrix, since it's Hermitian, taking advantage of eigh functon!
+    for j in prange(matrix_size):
+        for k in range(j+1):
+            # j,k entry in H
+            H[j,k]=constructHamiltonianEntry(j,k,theta_x,theta_y,matrixV)
+    return H
+
+# Compute the value for a single entry of the Hamiltonian matrix, which is a summation... 
+# @njit(boundscheck=True)
+@njit()
+def constructHamiltonianEntry(indexJ,indexK,theta_x,theta_y,matrixV):
+    value = 0
+
+    for n in range(-POTENTIAL_MATRIX_SIZE, POTENTIAL_MATRIX_SIZE+1):
+        if deltaPBC(indexJ,indexK-n):
+            for m in range(-POTENTIAL_MATRIX_SIZE, POTENTIAL_MATRIX_SIZE+1):
+                value += matrixV[m+POTENTIAL_MATRIX_SIZE,n+POTENTIAL_MATRIX_SIZE]*mn_LUT[m+POTENTIAL_MATRIX_SIZE,n+POTENTIAL_MATRIX_SIZE]*mj_LUT[m+POTENTIAL_MATRIX_SIZE,indexJ]*mTheta_LUT[m+POTENTIAL_MATRIX_SIZE,theta_y]/mTheta_LUT[n+POTENTIAL_MATRIX_SIZE,theta_x]
+    return value
+
+# a simplified "original" version of constructing the entries, a bit easier to understand what's going on than above
+# @njit()
+def constructHamiltonianEntryOriginal(indexJ,indexK,theta_x,theta_y,matrixV):
+    value = 0
+    
+    for n in range(-POTENTIAL_MATRIX_SIZE, POTENTIAL_MATRIX_SIZE+1):
+        if deltaPBC(indexJ,indexK-n):
+            for m in range(-POTENTIAL_MATRIX_SIZE, POTENTIAL_MATRIX_SIZE+1):
+                potentialValue = matrixV[m+POTENTIAL_MATRIX_SIZE,n+POTENTIAL_MATRIX_SIZE]
+                exp_term = (1/NUM_STATES)*((-PI/2)*(m**2+n**2)-IMAG*PI*m*n+IMAG*(m*theta_y-n*theta_x)-IMAG*m*2*PI*indexJ)
+                value +=potentialValue*np.exp(exp_term)
+
+                # alternatively break exponential into pieces, (slower, for testing)
+                # exp_termONE = np.exp(-PI*(m**2+n**2)/(2*NUM_STATES))
+                # exp_termTWO = np.exp(-IMAG*PI*m*n/NUM_STATES)
+                # exp_termTHREE = np.exp(IMAG*(m*theta_y-n*theta_x)/NUM_STATES)
+                # exp_termFOUR = np.exp(-IMAG*2*PI*m*indexJ/NUM_STATES)
+                # value += potentialValue*exp_termONE*exp_termTWO*exp_termTHREE*exp_termFOUR
+    return value
+
+# periodic boundary condition for delta-fx inner product <phi_j | phi_{k-n}> since phi_j=phi_{j+N}
+@njit()
+def deltaPBC(a,b,N=NUM_STATES):
+    modulus = (a-b)%N
+    if modulus==0: return 1
+    return 0
+
+# Construct the potential's fourier coefficients as in Yan Huo's thesis... 
+def constructPotential(size, mean=0, stdev=1):
+    # define a real, periodic gaussian potential over a size x size field where
+    # V(x,y)=sum V_{m,n} exp(2pi*i*m*x/L_x) exp(2pi*i*n*y/L_y)
+
+    # we allow V_(0,0) to be at V[size,size] such that we can have coefficients from V_(-size,-size) to V_(size,size). We want to have both negatively and
+    # positively indexed coefficients! 
+
+    V = np.zeros((2*size+1, 2*size+1), dtype=complex)
+    # loop over values in the positive +i,+j quadrant and +i,-j quadrant, assigning conjugates at opposite quadrants
+    for i in range(size + 1):
+        for j in range(-size, size + 1):
+            # Real and imaginary parts
+            real_part = np.random.normal(0, stdev)
+            imag_part = np.random.normal(0, stdev)
+            
+            # Assign the complex value 
+            V[size + i, size + j] = real_part + IMAG * imag_part
+            
+            # Enforce the symmetry condition, satisfy that V_{i,j}^* = V_{-i,-j}
+            if not (i == 0 and j == 0):  # Avoid double-setting the origin
+                V[size - i, size - j] = real_part - IMAG * imag_part
+
+    # set origin equal to a REAL number! (DC OFFSET)
+    # V[size, size] = np.random.normal(0, stdev) + 0*IMAG 
+
+    # Set DC offset to zero so avg over real-space potential = 0
+    V[size, size] = 0
+
+    return V
+
+# alternate way to construct potential (experimental)
+def constructScatteringPotentialv1(size):
+    # first pull numScatterers random xy locations and intensities
+    VReal = np.zeros((2*size+1, 2*size+1), dtype=complex)
+    for i in range(2*size+1):
+        for j in range(2*size+1):
+            VReal[i,j]=np.random.normal(0, 1)
+
+    fourier_transformed = np.fft.fft2(VReal)
+    # Shift the zero-frequency component to the center of the matrix
+    centered_fourier = np.fft.fftshift(fourier_transformed)
+
+    return centered_fourier
+
+# method of constructing potential as in 2019 paper
+def constructScatteringPotentialv2(size):
+    totalScatterers = 16*NUM_STATES
+    # first pull numScatterers random xy locations and intensities
+    L = np.sqrt(2*PI*NUM_STATES) # take ell^2 =1
+    deltaQ = 2*PI/L
+    LSquared = L**2
+
+    x_positions = np.random.uniform(0, L, totalScatterers)
+    y_positions = np.random.uniform(0, L, totalScatterers)
+    intensities = np.random.choice([-1,1],size=totalScatterers)
+
+    V = np.zeros((2*size+1, 2*size+1), dtype=complex)
+
+    # Generate Each V_mn in the entire grid
+    for m in range(size + 1):
+        for n in range(-size, size + 1):
+            #we must now take the sum over all scatterers...
+            accumulator = 0
+            for imp in range(totalScatterers):
+                x = x_positions[imp]
+                y = y_positions[imp]
+                accumulator += intensities[imp]*np.exp(-IMAG*deltaQ*(m*x+n*y))
+
+            accumulator = accumulator/LSquared #??????
+            V[size + m, size + n] = accumulator
+            V[size - m, size - n] = np.conjugate(accumulator)
+    
+    V[size,size]=0
+
+    return V
+
+# run a full iteration of the simulation. that is, compute the potential, then
+# construct the hamiltonian and find the eigenvalues for a mesh of theta_x, theta_y's.
+def fullSimulationGrid(thetaResolution=10,visualize=False):
+    # for a given iteration, define a random potential...
+    V = constructPotential(POTENTIAL_MATRIX_SIZE)
+    # V = constructScatteringPotential(POTENTIAL_MATRIX_SIZE)
+    # V = constructScatteringPotentialv2(POTENTIAL_MATRIX_SIZE)
+
+    # next, loop over theta_x theta_y
+    thetas = np.linspace(0, 2*PI, num=thetaResolution, endpoint=True)
+    # print(thetas)
+    delTheta = thetas[1]
+    print(delTheta)
+
+    eigGrid = np.zeros((thetaResolution,thetaResolution),dtype=object)
+    eigValueGrid = np.zeros((thetaResolution,thetaResolution),dtype=object)
+    for indexONE in range(thetaResolution):
+        for indexTWO in range(thetaResolution):
+            # H = constructHamiltonian(NUM_STATES,thetas[indexONE],thetas[indexTWO], V) 
+            H = constructHamiltonian(NUM_STATES,indexONE,indexTWO, V) 
+
+            eigs, eigv = np.linalg.eigh(H,UPLO="L")
+            # eigs, eigv = scipy.linalg.eigh(H,driver="evd")
+
+            eigGrid[indexONE,indexTWO]=[thetas[indexONE],thetas[indexTWO],eigs]
+            eigValueGrid[indexONE,indexTWO]=eigv
+
+    cherns=[]
+    for i in range(NUM_STATES):
+        chernNum = computeChernGridV2(i,eigValueGrid,delTheta,thetaResolution)
+        # print(chernNum)
+        if np.abs(chernNum.imag) < 1e-8:
+            chernNum = chernNum.real
+        cherns.append(chernNum)
+
+    cherns = np.round(cherns,decimals=3)
+
+    if visualize:
+        print(cherns)
+        print("Sum",sum(cherns))
+        plotEigenvalueMeshHelper(eigGrid,thetas,thetaResolution)
+
+    return cherns
+
+# Helper function to plot in 3D the eigenvalues for a grid of theta_x, theta_y
+def plotEigenvalueMeshHelper(grid,thetas,thetaResolution):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import random
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    colors = [tuple(random.random() for _ in range(3)) for _ in range(NUM_STATES)]  # Random RGB colors
+
+    # Plot each surface for each index of valueArray
+    for idx,color in enumerate(colors):  # Assuming valueArray has 3 elements
+        # Prepare data for this surface (x, y, valueArray[idx])
+        X, Y = np.meshgrid(thetas, thetas)
+        Z = np.array([[grid[i, j][2][idx] for j in range(thetaResolution)] for i in range(thetaResolution)])
+
+        # Plot the surface
+        ax.plot_surface(X, Y, Z, color=color, edgecolor='none', alpha=1.0)
+
+    # Set labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Show the plot
+    plt.show()
+
+def computeChernGridV2(stateNumber,grid,delTheta,thetaNUM):
+    accumulator = 0
+
+    for indexONE in range(thetaNUM-1):
+        for indexTWO in range(thetaNUM-1):
+            currentEigv00 = grid[indexONE,indexTWO][:,stateNumber]
+            currentEigv10 = grid[(indexONE+1),indexTWO][:,stateNumber]
+            currentEigv01 = grid[indexONE,(indexTWO+1)][:,stateNumber]
+            currentEigv11 = grid[(indexONE+1),(indexTWO+1)][:,stateNumber]
+
+            innerProductOne = np.vdot(currentEigv00,currentEigv10)
+            innerProductTwo = np.vdot(currentEigv10,currentEigv11)
+            innerProductThree = np.vdot(currentEigv11,currentEigv01)
+            innerProductFour = np.vdot(currentEigv01,currentEigv00)
+
+
+            # we don't need to normalize anything since we don't care about the real part of the log! 
+
+            # simple implementation, do this and throw away resulting imag part
+            # berryPhase = -IMAG*np.log(innerProductOne*innerProductTwo*innerProductThree*innerProductFour)
+
+            # alternatively use arctangent to get only the part we care about!
+            # can break up or do entire thing... 
+
+            # product = innerProductOne*innerProductTwo*innerProductThree*innerProductFour
+            # berryPhase=np.arctan2(product.imag,product.real)
+            berryPhase = np.arctan2(innerProductOne.imag,innerProductOne.real) + np.arctan2(innerProductTwo.imag,innerProductTwo.real) + np.arctan2(innerProductThree.imag,innerProductThree.real) + np.arctan2(innerProductFour.imag,innerProductFour.real)
+
+            # phase-shift to range of [-PI,PI]
+            while berryPhase>PI:
+                # print(berryPhase)
+                berryPhase -= 2*PI
+            
+            while berryPhase<-PI:
+                # print(berryPhase)
+                berryPhase += 2*PI
+
+            accumulator += berryPhase
+
+    return (accumulator/(2*PI)+1/NUM_STATES)
+
+def ensembleRun(n_iters,numTheta,csv_file):
+    # Open the CSV file in write mode
+    with open(csv_file, mode="a", newline="") as file:
+        writer = csv.writer(file)
+
+        # if file.tell() == 0:  # Check if the file is empty
+            # writer.writerow(["State 1", "State 2", "State 3", "State 4", "State 5", "State 6", "State 7", "State 8"])
+
+        # Loop to generate and write arrays
+        for i in range(n_iters):  # Adjust the range for the desired number of rows
+            chern_numbers = fullSimulationGrid(numTheta,visualize=False)
+            writer.writerow(chern_numbers)
+            file.flush()  
+            print("ITERATION ", i, "COMPLETE")
+
+# function to help time something like the hamiltonian construction
+def timing():
+    NUM=1000
+    time = timeit.timeit("constructHamiltonian(256,3,7,V)", 'from __main__ import constructHamiltonian, PI, V', number=NUM)
+    # time = timeit.timeit("optimizedConstructHamiltonian(16,8,2,V)", 'from __main__ import optimizedConstructHamiltonian, V', number=NUM)
+    # time = timeit.timeit("fullSimulationGrid(NUM_THETA,visualize=False)", 'from __main__ import fullSimulationGrid, NUM_THETA', number=NUM)
+    
+    print(f"Execution time: {time} seconds")
+    print("Time per Call:", time/NUM)
+
+if __name__ == "__main__":
+    # fullSimulationGrid(NUM_THETA,visualize=True)
+
+    V = constructPotential(POTENTIAL_MATRIX_SIZE)
+    constructHamiltonian(256,3,7,V)
+    timing()
+
+    # Comment the following lines for running an ensemble!
+    # csv_file = "/scratch/gpfs/ed5754/iqheFiles/output.csv"
+    # csv_file = "output.csv"
+    # ensembleRun(100,30,csv_file)
+
+
+    # Following lines are for visualizing the V_mn potential strength, translated back to real space!
+    # pot = constructPotential(10)
+    # pot= constructScatteringPotentialv2(10)
+    # shifted = np.fft.ifftshift(pot)
+    # real = np.fft.ifft2(shifted).real
+
+    # plt.figure(figsize=(8, 6))
+    # plt.imshow(real, extent=[0, real.shape[1], 0, real.shape[0]],
+    #            origin='lower', cmap='inferno')
+    # plt.colorbar(label="Magnitude of Real-Space Potential")
+    # plt.title("Gaussian White Noise Potential in Real Space")
+    # plt.xlabel("x")
+    # plt.ylabel("y")
+    # plt.show()
